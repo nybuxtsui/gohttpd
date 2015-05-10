aad63d1a8229429a0c8ce36210679d57420b802b
xubin <nybux.tsui@gmail.com>
2015-05-10 08:16:52 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+const HTDOCS = "htdocs"
+
+func main() {
+	if fi, err := os.Stat(HTDOCS); os.IsNotExist(err) {
+		log.Fatalln("htdocs not exist")
+	} else if !fi.IsDir() {
+		log.Fatalln("htdocs is not dir")
+	}
+
+	addr := flag.String("addr", ":8080", "http server listen address, format: [ip]:port")
+	http.Handle("/", http.FileServer(http.Dir(HTDOCS)))
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
